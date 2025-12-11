29f678f05fee189114c6d42ab1064becba1c106b
Renn Valo <renn.co@gmail.com>
2025-12-11 16:57:59 -0700

    updated resume for the page - still a long way to go

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/resume', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'resume.html'));
+});
+
 app.get('/project/:slug', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'project.html'));
 });
